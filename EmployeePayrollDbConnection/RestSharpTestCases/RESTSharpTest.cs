95ad23cb1b0a4a1b1fe52a68abebee68b9e679f0
hem_b <hemanthb93@gmail.com>
2022-07-01 22:53:35 +0530

    [hemanth]:Adds a new data to json using post method

--- EmployeePayrollDbConnection/RestSharpTestCases/RESTSharpTest.cs
+++ EmployeePayrollDbConnection/RestSharpTestCases/RESTSharpTest.cs
@@ -26,12 +26,29 @@ namespace RestSharpTestCases
             //Assert
             Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
             List<Employee> list = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
-            Assert.AreEqual(8, list.Count);
+            Assert.AreEqual(9, list.Count);
             foreach (Employee data in list)
             {
                 Console.WriteLine("{0,-5}{1,-15}{2,-10}", data.Id, data.Name, data.Salary);
 
             }
+        }
+        [TestMethod]
+        public void OnPostingEmployeeData_ShouldAddtoJsonServer()
+        {
+            client = new RestClient("http://localhost:4000");
+            //Arrange
+            RestRequest request = new RestRequest("/employees", Method.Post);
+            var body = new Employee { Name = "Krishnan", Salary = "45000" };
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            //Act
+            RestResponse response = client.Execute(request);
+            //Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+            Employee data = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Krishnan", data.Name);
+            Assert.AreEqual("45000", data.Salary);
+            Console.WriteLine(response.Content);
         }
     }
 }
